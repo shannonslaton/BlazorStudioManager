bab773a2c8976c1649f8f98dbc4f25ef25e052d1
Shannon Slaton <shannonslaton@live.com>
2021-07-23 16:41:42 -0400

    2

--- BlazorStudioManager/Client/Program.cs
+++ BlazorStudioManager/Client/Program.cs
@@ -14,23 +14,6 @@ namespace BlazorStudioManager.Client
 {
     public class Program
     {
-   //     public static async Task Main(string[] args)
-   //     {
-   //         var builder = WebAssemblyHostBuilder.CreateDefault(args);
-   //         builder.RootComponents.Add<App>("#app");
-			//builder.Services.AddTelerikBlazor();
-
-   //         builder.Services.AddHttpClient("BlazorStudioManager.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-   //             .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-   //         // Supply HttpClient instances that include access tokens when making requests to the server project
-   //         builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorStudioManager.ServerAPI"));
-
-   //         builder.Services.AddApiAuthorization();
-
-   //         await builder.Build().RunAsync();
-   //     }
-
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
