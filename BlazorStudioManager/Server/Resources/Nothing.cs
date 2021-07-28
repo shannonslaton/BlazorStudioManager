341106cc682bc4be14bd61caef107e751292dec1
Shannon Slaton <shannonslaton@live.com>
2021-07-28 19:03:28 -0400

    f

---
+++ BlazorStudioManager/Server/Resources/Nothing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStudioManager.Server.Resources
+{
+    public class Nothing
+    {
+        public int RecId { get; set; }
+    }
+}
